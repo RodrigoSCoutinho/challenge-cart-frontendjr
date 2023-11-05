a69eb146afc924d6bffb065a240493226b11200d
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-11-05 16:45:04 -0300

    feature: called api and create method for to search de products

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  // Chamada a API produtos e criacao de um metodo para ser chamado la no componente produto
+
+  constructor(private http : HttpClient) { }
+     getProduct(){
+        return this.http.get<any>("https://fakestoreapi.com/products")
+        .pipe(map((res:any) => {
+           return res
+        }))
+     }
 }
